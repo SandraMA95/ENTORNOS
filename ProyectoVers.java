7ec14d00075866132a706fffa2fc4d09914a5cf9
Sandra <saandreis.aramac@gmail.com>
2024-02-05 13:05:38 +0100

    Variables de clase

--- ProyectoVers.java
+++ ProyectoVers.java
@@ -1,5 +1,17 @@
+
+/**
+ * @author Sandra Macias Aragon
+ * @version 5.febrero.2024
+ * */
+
 package ControlVersion;
 
 public class ProyectoVers {
+	
+	private int a;
+	
+	private int b;
+	
+	private int answer;
 
 }
