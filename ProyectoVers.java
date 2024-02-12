2cfd7ce3fbc084042d9f8403e02bd955b125eabb
Sandra <saandreis.aramac@gmail.com>
2024-02-12 11:50:44 +0100

    Creado constructor con argumentos

--- ProyectoVers.java
+++ ProyectoVers.java
@@ -7,20 +7,28 @@
 package ControlVersion;
 
 public class ProyectoVers {
-	
+
 	private int a;
-	
+
 	private int b;
-	
+
 	private int answer;
 
-	
 	public ProyectoVers() {
-		
+
 		this.a = 0;
-		
+
 		this.b = 0;
-		
+
+		this.answer = 0;
+	}
+
+	public void ProyectoVers1(int a, int b, int answer) {
+
+		this.a = 0;
+
+		this.b = 0;
+
 		this.answer = 0;
 	}
 }
